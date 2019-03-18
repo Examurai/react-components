c8c113bfbde8c6036b34dd72e128d4eb5b45c909
Aydar <shimakenshi@gmail.com>
2019-03-18 13:24:36 +0500

    Обновлен пример использования Router

--- my-app-redux-router/src/App.js
+++ my-app-redux-router/src/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from "react";
+import { Route, Switch, Router } from "react-router";
 import Chart from "./containers/Chart/Chart";
+import Empty from "./components/Empty/Empty";
 import "./App.css";
+import { history } from "./store/index";
 
 export default class App extends Component {
   render() {
-    return <Chart />;
+    return (
+      <Router history={history}>
+        <Switch>
+          <Route path="/chart" component={Chart} />
+          <Route path="/" component={Empty} />
+        </Switch>
+      </Router>
+    );
   }
 }
